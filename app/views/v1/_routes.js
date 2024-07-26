4882ea7e07edc4d8bcb547da44fe93eadc5fb611
Iona Smith <iona.smith3@nhs.net>
2024-07-26 08:30:40 +0100

    contact number screen and content

--- app/views/v1/_routes.js
+++ app/views/v1/_routes.js
@@ -15,5 +15,18 @@ router.post(/start/, (req, res) => {
     res.redirect('date-of-birth')
 });
 
+// Did the deceased have any dependents page
+router.post('/deceased-dependents-edit', (req, res) => {
+
+    const contact = req.session.data['dependentPage']
+
+    if (contact == 'yes') {
+        res.redirect('deceased-dependents-list')
+    } else {
+        res.redirect('deceased-date-of-birth')
+
+    }
+});
+
 
 module.exports = router;

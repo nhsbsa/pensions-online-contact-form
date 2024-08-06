32b02aa662fdfe9c0281d42198a63a9b45154c2a
SushmitaJ21 <122446961+SushmitaJ21@users.noreply.github.com>
2024-08-06 10:33:09 +0100

    V2 of the prototype

---
+++ app/views/v2/_routes.js
@@ -0,0 +1,126 @@
+// External dependencies
+const e = require('express');
+const express = require('express');
+const router = express.Router();
+
+// Add your routes here - above the module.exports line
+
+// ========================================================================
+// MAIN ONLINE CONTACT FORM JOURNEY
+// ========================================================================
+
+
+// Start page
+router.post(/start/, (req, res) => {
+    res.redirect('date-of-birth')
+});
+
+
+//informant-postcode-no-result
+router.post('/informant-postcode-no-result/', (req, res) => {
+
+    const contract = req.session.data['noAddress']
+
+    if (contract == 'change') {
+        res.redirect('informant-postcode-lookup')
+    } else {
+        res.redirect('informant-full-address')
+
+    }
+});
+
+//informant-postcode-lookup
+router.post('/informant-postcode-lookup', (req, res) => {
+
+    let postcode = req.session.data['enterPostcode']
+
+    if (postcode == 'ABC 123') {
+        res.redirect('informant-postcode-no-result')
+    } else {
+        res.redirect('informant-address-select')
+
+    }
+});
+
+//eligibility-active-employment page
+router.post('/eligibility-active-employment', (req, res) => {
+
+    const contract = req.session.data['employment']
+
+    if (contract == 'yes') {
+        res.redirect('eligibility-contact-employer')
+    } else {
+        res.redirect('eligibility-recieving-pension')
+
+    }
+});
+
+//eligibility-tell-us-once page
+router.post('/eligibility-tell-us-once', (req, res) => {
+
+    const contract = req.session.data['tellOnce']
+
+    if (contract == 'yes') {
+        res.redirect('eligibility-tell-us-once-reference')
+    } else {
+        res.redirect('informant-name')
+
+    }
+});
+
+//informant-uk-main-residence page
+router.post('/informant-uk-main-residence', (req, res) => {
+
+    const select = req.session.data['main']
+
+    if (select == 'yes') {
+        res.redirect('informant-postcode-lookup')
+    } else {
+        res.redirect('informant-international-address')
+
+    }
+});
+
+//deceased-dependants-confirm page
+router.post('/deceased-dependants-confirm', (req, res) => {
+
+    const contract = req.session.data['dependents']
+
+    if (contract == 'yes') {
+        res.redirect('deceased-dependants-list')
+    } else {
+        res.redirect('deceased-uk-main-residence')
+
+    }
+});
+
+//deceased-uk-main-residence page
+router.post('/deceased-uk-main-residence', (req, res) => {
+
+    const check = req.session.data['uk']
+
+    if (check == 'yes') {
+        res.redirect('deceased-postcode-lookup')
+    } else {
+        res.redirect('deceased-international-address')
+
+    }
+});
+
+//deceased-postcode-lookup
+router.post('/deceased-postcode-lookup', (req, res) => {
+
+    let postcode = req.session.data['enter']
+
+    if (postcode == 'ABC 123') {
+        res.redirect('deceased-postcode-no-result')
+    } else {
+        res.redirect('deceased-address-select')
+
+    }
+});
+
+
+
+
+module.exports = router;

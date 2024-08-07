481b097f657937d7786467ba6a0f61ff6f0e19fe
Iona Smith <iona.smith3@nhs.net>
2024-08-07 13:29:13 +0100

    routes updated for v2

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,10 @@ router.post('/clear-session-data/', (req, res) => {
 // Version Routes Files Below
 // =======================================
 
+//Routes for v1
 router.use('/v1', require('./views/v1/_routes'));
+
+//Routes for v2 
+router.use('/v2', require('./views/v2/_routes'));
 
 module.exports = router;
